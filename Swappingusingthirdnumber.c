6042e8479c8365b0d022ca270e5ddbb0a419c04d
yashgautam-codes <yashgautam8368@gmail.com>
2026-08-12 14:36:03 +0530

    Add files via upload

---
+++ Swappingusingthirdnumber.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter First Number 'a' :- ");
+    scanf("%d",&a);
+    int b;
+    printf("Enter Second Number 'b' :- ");
+    scanf("%d",&b);
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    printf("New value of a = %d",a);
+     printf("\nNew value of b = %d",b);
+    return 0;
+}
